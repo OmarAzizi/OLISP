bc5a092d07cf2568ba8bf8450a7effb429a54aca
OmarAzizi <oalazizi75@gmail.com>
2024-04-12 01:17:14 +0300

    Making OLISP more portable by detecting if we are compiling on a Windows machine

--- lisp.c
+++ lisp.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    If we are compiling on Windows compile these function
+*/
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake `readline` function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake `add_history` function */
+void add_history(char* unused) {}
+
+/* 
+    Otherwise include the editline headers 
+*/
+#else
 /* 
     This library will help us create a better interactive prompt on linux by allowing editing 
     and recording the history of inputs so that they can be retrieved with the up and down arrows 
 */
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
     /* Print Version and Exit Information */
